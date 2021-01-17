ce647707616ad7dd290d836758a4fa877d243019
Ariel Lopes De Souza <ariel.edit@gmail.com>
2021-01-17 10:25:47 -0400

    front-mobile

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,33 +1,32 @@
+import React from 'react';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    OpenSans_400Regular, 
+    OpenSans_400Regular,
     OpenSans_700Bold
   });
 
-  if(!fontsLoaded){
-    return <AppLoading />;  
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   return (
     <View style={styles.container}>
-     <Header />
-     <Home />
-      <StatusBar style="auto" />
-    </View>
+          <StatusBar style="auto" />
+         <Routes />
+          </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
+  
   },
 });
--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,31 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-export default function Header() {
+function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+      navigation.navigate('Home');
+
+  }
   return (
+    <TouchableWithoutFeedback onPress={handleOnPress}>
     <View style={styles.container}>
       <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+      <Text style={styles.text}> DS Deliver</Text>
+     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#DA5C5C',
-    height: 90,
+    height:90,
     paddingTop: 50,
     flexDirection: 'row',
-    justifyContent: 'center'
+    justifyContent: 'center',
+
   },
   text: {
     fontWeight: 'bold',
-    fontSize: 18,
-    lineHeight: 25,
-    letterSpacing: -0.24,
-    color: '#FFF',
-    marginLeft: 15,
-    fontFamily: 'OpenSans_700Bold'
+fontSize: 18,
+lineHeight: 25,
+letterSpacing: -0.24,
+color: '#FFF',
+marginLeft: 15,
+fontFamily: 'OpenSans_700Bold',
+
+
+
   }
 });
+export default Header;
